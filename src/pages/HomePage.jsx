f4bf4b9cc5740352ce2e10daf33ef79f096d1a24
Aruminiya <leo80103@gmail.com>
2024-08-27 17:34:48 +0800

    初步測試 material ui 成功

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,17 @@
+import Button from '@mui/material/Button';
+import Alert from '@mui/material/Alert';
+
 export default function HomePage() {
   return (
     <>
       <h1>歡迎來到首頁</h1>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+
+      <Alert severity="success">
+        Here is a gentle confirmation that your action was successful.
+      </Alert>
     </>
   )
 };
