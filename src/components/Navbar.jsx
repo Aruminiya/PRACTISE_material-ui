4491eda57201ecbfb41bcbb7111eabb4cf3133c3
Aruminiya <leo80103@gmail.com>
2024-08-27 18:21:26 +0800

    切版中

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import Link from '@mui/material/Link';
-import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
-    <nav id="nav" className={styles.nav}>
-      <div className='links'>
-        <Link href="/">首頁</Link>
-        <Link href="/about">關於我們</Link>
-      </div>
+    <nav id="nav">
+      <ul className='links'>
+        <li>
+          <Link href="/">首頁</Link>
+        </li>
+        <li>
+          <Link href="/about">關於我們</Link>
+        </li>
+      </ul>
     </nav>
   )
 };
